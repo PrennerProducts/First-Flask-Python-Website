bbbdd9aee7551ea4336faa3a93803f95d3b04b31
Lukas <prenner.products@gmail.com>
2021-05-06 21:06:30 +0200

    try

--- website.py
+++ website.py
@@ -1,18 +1,11 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hallo das ist die main page <h1>Hallo<h1>"
-
 @app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+def home(name):
+    return render_template("index.html", content=["Lukas", "Maria", "Elroy", "Mogli"])
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run()
